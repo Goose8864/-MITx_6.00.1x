70f46a6006bdf8028fff9747aa3dcc79ca4f085d
Ryan <ryragusa@gmail.com>
2019-10-14 12:03:20 -0500

    add new python scripts

---
+++ evenOdd.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  7 22:30:23 2019
+
+@author: Ryan
+"""
+
+x = int(input('Enter an integer: '))
+if x%2 == 0:
+    print('')
+    print('Even')
+else:
+    print('')
+    print('Odd')
+print('Done with conditional')
---
+++ hello.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  2 13:04:28 2019
+
+@author: Ryan
+"""
+
+x= int(input('Enter an integer: '))
+if x%2 == 0:
+    print('')
+    print('Even')
+else:
+    print('')
+    print('Odd')
+print('Done with conditional')
+        
