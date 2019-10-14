c96bb07dc077a8ad1103ebef4d70606664fc14f2
Ryan <ryragusa@gmail.com>
2019-10-14 12:34:52 -0500

    added comments

--- evenOdd.py
+++ evenOdd.py
@@ -4,12 +4,15 @@ Created on Mon Oct  7 22:30:23 2019
 
 @author: Ryan
 """
-
+# Sets x to the user input. User input will be an int.
 x = int(input('Enter an integer: '))
+# If user input(x) is divisible by two with no remainder then print "even"
 if x%2 == 0:
     print('')
     print('Even')
+# If user input is not divisible by two with no remainder, then print "Odd"
 else:
     print('')
     print('Odd')
+# When conditional has finished print "Done with conditional"
 print('Done with conditional')
